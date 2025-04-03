5b4cbcf3819bec6e3c4697146057870fc54d91ff
Alex Akasaka <demazian0p@gmail.com>
2025-04-03 14:43:38 -0700

    pythonwip2

--- project2.py
+++ project2.py
@@ -12,3 +12,18 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+#multithreading function
+def multi_core_prime_search(duration, num_processes):
+    manager = multiprocessing.Manager()
+    # Shared variable to store the highest prime found.
+    highest_prime = manager.Value('i', 0)
+    start_time = time.time()
+    processes = []
+    for i in range(num_processes):
+        I = multiprocessing.Process(target=worker, args=(i, num_processes))
+        processes.append(p)
+        i.start()
+    for i in processes:
+        i.join()
+    return highest_prime.value 
